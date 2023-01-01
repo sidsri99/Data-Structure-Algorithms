4e976909d183ebd9e440253550487573dd275fb6
sidsri99 <1sid0sri@gmail.com>
2023-01-01 17:14:40 +0530

    Adding Files

---
+++ Graphs/Graphs.md
@@ -0,0 +1,9 @@
+# Graphs
+### Graph is a non-linear data structure consisting of vertices(V) and edges(E).
+
+## Vertices
+#### Vertices or Nodes are fundamental unit of a graph.
+
+## Edges
+#### Edges are lines connecting nodes of a graph
+
---
+++ Graphs/Representation/Matrix.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Matrix
+{
+    public static void main(String args[]){
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int row,column;
+        System.out.println("Enter number of rows");
+        row=Integer.parseInt(br.readLine);
+        System.out.println("Enter number of columns");
+        column=Integer.parseInt(br.readLine);        
+        int matrix[][]=new int[row+1][column+1];
+    }
+}
