3baee72d43a9a14241c2b7beb9df03a8dbb0a24d
sidsri99 <1sid0sri@gmail.com>
2023-01-04 23:40:18 +0530

    Starting with Arrays question 1

---
+++ Arrays/Problem1.java
@@ -0,0 +1,21 @@
+/*
+    Leaders in an array:
+
+    Given an array A of positive integers.
+    Task is to find the leaders in the array.
+    An element of array is leader if it is greater than or equal to all the elements to its right side.
+    The rightmost element is always a leader. \
+    
+    Example 1:
+    Input:
+    n = 6
+    A[] = {16,17,4,3,5,2}
+    Output: 17 5 2
+    
+    Example 2:
+    Input:
+    n = 5
+    A[] = {1,2,3,4,0}
+    Output: 4 0
+*/
+
