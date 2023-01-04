68e0e65e1728a639e33c2e5762544742afe25ea9
sidsri99 <1sid0sri@gmail.com>
2023-01-05 00:03:24 +0530

    Array Problem1: Leaders in array

--- Arrays/Problem1.java
+++ Arrays/Problem1.java
@@ -19,3 +19,60 @@
     Output: 4 0
 */
 
+import java.io.*;
+import java.util.*;
+class Problem1{
+    ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        ArrayList<Integer> revlist=new ArrayList<Integer>();
+        int max=arr[n-1];
+        list.add(max);
+        for(int i=n-2;i>=0;i--){
+            int temp=arr[i];
+            if(temp>=max){
+                max=temp;
+                list.add(max);
+            }
+        }
+        for(int i=list.size()-1 ; i>=0 ; i--){
+            revlist.add(list.get(i));
+        }
+        return revlist;
+    }
+    public static void main(String args[])throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Problem1 obj=new Problem1();
+        int t=Integer.parseInt(br.readLine().trim());
+        while(t-->0){
+            int n=Integer.parseInt(br.readLine().trim());
+            int arr[]=new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            StringBuffer str=new StringBuffer();
+            ArrayList<Integer> res=new ArrayList<Integer>();
+
+            res=obj.leaders(arr,n);
+
+            for(int i=0;i<res.size();i++){
+                str.append(res.get(i)+" ");
+            }
+            System.out.println(res);
+        }
+    }
+}
+
+/*
+    Output:
+    >javac Problem1.java
+    >java Problem1
+    >2
+    >6
+    >16 17 4 3 5 2
+    [17, 5, 2]
+    >5
+    >1 2 3 4 0
+    [4, 0]
+*/
