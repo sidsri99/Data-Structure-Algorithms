d3f6890b8be8f7ede7a0e95b2d944713ee159766
sidsri99 <1sid0sri@gmail.com>
2023-01-05 22:51:51 +0530

    Lambda expression in Java syntax

---
+++ lambda.java
@@ -0,0 +1,7 @@
+/*
+    Lambda expression in Java
+    Syntax:
+    parameter -> expression
+    (parameter1 , parameter2) -> expression
+    (parameter1 , parameter2) -> { code block }  // can use return in code block to return a value
+*/
