761848a1dd73615bc2282edfae8cdc9acc8f48da
sidsri99 <1sid0sri@gmail.com>
2023-03-25 18:28:45 +0530

    Problem 10: Summation of primes: Sieve of Eratosthenes

--- Practice_480Q/Problem10.java
+++ Practice_480Q/Problem10.java
@@ -4,6 +4,7 @@
     Find sum of all the primes below n
 */
 import java.io.*;
+import java.util.*;
 class Problem10{
     boolean isPrime(int n){
         for(int i=2;i*i<=n;i++){
@@ -19,13 +20,38 @@ class Problem10{
         }
         return sum;
     }
+    /*
+        Adding Sieve of Erathosthenes logic for better time complexity
+    */
+    
+    long SumPrimeNew(int n){
+        long sum = 0;
+        int arr[]=new int[n+1];
+        Arrays.fill(arr,1);
+        for(int i = 2;i*i<=n;i++){
+            if(arr[i]==1){
+                for(int j=i*i; j<=n; j=j+i){
+                    arr[j]=0;
+                }
+            }
+        }
+        for(int i = 2 ; i < n ; i++){
+            if(arr[i]==1){
+                sum +=i;
+            }
+        }
+        return sum;
+    }
     public static void main(String args[])throws IOException{
         Problem10 obj=new Problem10();
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         while(t-->0){
             int num=Integer.parseInt(br.readLine());
+            /*
             System.out.println(obj.SumPrime(num));
+            */
+            System.out.println(obj.SumPrimeNew(num));
         }
     }
 }
@@ -33,7 +59,9 @@ class Problem10{
 Output:
 >javac Problem10.java
 >java Problem10
->4
+>5
+>10
+17
 >17
 41
 >2001
