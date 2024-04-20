b566f30524adb90db2b44816f8f080591d06ccc4
sidsri99 <1sid0sri@gmail.com>
2024-04-20 19:38:18 +0530

    P2: Power of Three : Recursion V1

--- Recursion/Problem2.java
+++ Recursion/Problem2.java
@@ -4,11 +4,23 @@
 
 import java.io.*;
 class Problem2{
+	//Better
+	public boolean isPowerOfThree(int n) {
+        if(n<=0) return false;
+		
+		if(n==1) return true;
+		
+		if(n%3!=0) return false;
+		
+		return isPowerOfThree(n/3);
+    }
+	/*
 	public boolean isPowerOfThree(int n) {
         if(n == 1) return true;
         else if( n == 0) return false;
         return (n == 1 || ( (n%3 == 0) && isPowerOfThree(n/3) ));
     }
+	*/
 	public static void main(String args[])throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Problem2 obj= new Problem2();
