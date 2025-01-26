45d18af90588d4ae9d5b14e420de17aa9987a745
Siddharth Srivastava <39755416+sidsri99@users.noreply.github.com>
2025-01-26 21:49:01 +0530

    BFS or Breadth First Search

---
+++ Graphs/BFS.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.util.*;
+
+// Driver code
+class BFS {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testcases = sc.nextInt(); // Taking number of test cases as input
+
+        while (testcases-- > 0) {
+            int V = sc.nextInt(); // Number of vertices
+            int E = sc.nextInt(); // Number of edges
+
+            // Initialize adjacency list
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) {
+                adj.add(new ArrayList<>()); // Create a new list for each vertex
+            }
+
+            // Add edges to the adjacency list
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                adj.get(u).add(v); // Adding edge u -> v
+                adj.get(v).add(u); // Adding edge v -> u (undirected graph)
+            }
+
+            // Create Solution object and call bfsOfGraph
+            Solution obj = new Solution();
+            ArrayList<Integer> result = obj.bfsOfGraph(V, adj);
+
+            // Print the result
+            for (int node : result) {
+                System.out.print(node + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close(); // Close the scanner
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        ArrayList<Integer> al=new ArrayList<Integer>();
+        boolean visited[]=new boolean[adj.size()];
+        Queue<Integer> que=new LinkedList<Integer>();
+        visited[0] = true;
+        que.add(0);
+        while(!que.isEmpty()){
+            int tmp = que.poll();
+            al.add(tmp);
+            for(int num: adj.get(tmp)){
+                if(!visited[num]){
+                    visited[num] = true;
+                    que.add(num);
+                }
+            }
+        }
+        return al;
+    }
+}
