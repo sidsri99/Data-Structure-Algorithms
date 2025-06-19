f38646019f4997b4777f556e1cc93cf9d0912a05
Siddharth Srivastava <39755416+sidsri99@users.noreply.github.com>
2025-06-19 11:30:34 +0530

    P64: Partition Array Such That Maximum Difference Is K

---
+++ Arrays/Problem64.java
@@ -0,0 +1,61 @@
+/**
+
+P64: Partition Array Such That Maximum Difference Is K
+
+Example 1:
+
+Input: nums = [3,6,1,2,5], k = 2
+Output: 2
+Explanation:
+We can partition nums into the two subsequences [3,1,2] and [6,5].
+The difference between the maximum and minimum value in the first subsequence is 3 - 1 = 2.
+The difference between the maximum and minimum value in the second subsequence is 6 - 5 = 1.
+Since two subsequences were created, we return 2. It can be shown that 2 is the minimum number of subsequences needed.
+Example 2:
+
+Input: nums = [1,2,3], k = 1
+Output: 2
+Explanation:
+We can partition nums into the two subsequences [1,2] and [3].
+The difference between the maximum and minimum value in the first subsequence is 2 - 1 = 1.
+The difference between the maximum and minimum value in the second subsequence is 3 - 3 = 0.
+Since two subsequences were created, we return 2. Note that another optimal solution is to partition nums into the two subsequences [1] and [2,3].
+Example 3:
+
+Input: nums = [2,2,4,5], k = 0
+Output: 3
+Explanation:
+We can partition nums into the three subsequences [2,2], [4], and [5].
+The difference between the maximum and minimum value in the first subsequences is 2 - 2 = 0.
+The difference between the maximum and minimum value in the second subsequences is 4 - 4 = 0.
+The difference between the maximum and minimum value in the third subsequences is 5 - 5 = 0.
+Since three subsequences were created, we return 3. It can be shown that 3 is the minimum number of subsequences needed.
+
+*/
+
+class Problem64 {
+    public int partitionArray(int[] nums, int k) {
+        int count = 1,tmp;
+        Arrays.sort(nums);
+        tmp = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            if((nums[i]-tmp) > k){
+                count += 1;
+                tmp = nums[i];
+            }   
+        }
+        return count;
+    }
+}
+
+/*
+
+Input
+nums =
+[3,1,3,4,2]
+k =
+0
+Output
+4
+
+*/
